89bb8257be389789cad3633974b7d34f59b74897
Timo Wurmbäck <62293162+timowww1@users.noreply.github.com>
2022-07-21 20:28:10 +0200

    update2107

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6fea17ca9b6d5ad67940ffcd4785a164",
-"index.html": "5e3dcadff63be507e989fbaa5da0124f",
-"/": "5e3dcadff63be507e989fbaa5da0124f",
-"main.dart.js": "0a5a2027fdc2c016245c2d9825e9f4ea",
+"index.html": "336b9abfbbe3dcf4a20292d41a251b13",
+"/": "336b9abfbbe3dcf4a20292d41a251b13",
+"main.dart.js": "dacfc9f15cb5703572e36f0117c0c816",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
