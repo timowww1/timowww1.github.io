bb912b00aaca92f8d5f5a23295ff6771c5cbdc15
Timo Wurmbäck <62293162+timowww1@users.noreply.github.com>
2022-07-22 19:10:30 +0200

    update 2207 v3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6fea17ca9b6d5ad67940ffcd4785a164",
-"index.html": "5869aded8477679094b8db0d7f99da4d",
-"/": "5869aded8477679094b8db0d7f99da4d",
-"main.dart.js": "c46b7cd0914a1676850bda32364ba5c3",
+"index.html": "30f6908015a7c1d5e542f1b29c58308e",
+"/": "30f6908015a7c1d5e542f1b29c58308e",
+"main.dart.js": "2bbc134b1546fbe778d43d3d7b9c0f14",
 "flutter.js": "3688efe0a39e59781b4f95efbd6b5b62",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
