b3999d8ac8f9ac9fedbe3badf6d0b0e5f406018c
Timo Wurmbäck <62293162+timowww1@users.noreply.github.com>
2022-08-10 19:42:51 +0200

    update1008_2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5514a34cc78a50b388479aa38d752a3e",
-"/": "5514a34cc78a50b388479aa38d752a3e",
-"main.dart.js": "612d7dda8abe0e578e433e92427dfb51",
+"index.html": "8f6b659a4af4f09c11b23b00d6ae8fd4",
+"/": "8f6b659a4af4f09c11b23b00d6ae8fd4",
+"main.dart.js": "757b69288d4263777a553ea4a486ffd3",
 "manifest.json": "4afb8702145c9fda727006fda7f2639e",
 "version.json": "6fea17ca9b6d5ad67940ffcd4785a164"
 };
